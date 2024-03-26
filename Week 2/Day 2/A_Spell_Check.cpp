c18d28dbc715c34d76e92ef12352ad88dace3ff4
krishna3052002 <shukrishna23@gmail.com>
2024-03-26 23:58:22 +0600

    Day 2 Added

---
+++ Week 2/Day 2/A_Spell_Check.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        string name="Timur";
+       
+       
+        if(name.size()!=s.size())
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            int count=0;
+            for(char c: name)
+            {
+                bool flag=false;
+                for(int i=0;i<n;i++)
+                {
+                    cout<<s[i]<<endl;
+                    if(c==s[i])
+                    {
+                      flag=true;
+                      break;
+                    }
+                    
+                }
+                if(flag==false)
+                {
+                    cout<<"NO"<<endl;
+                    break;
+                }
+                count++;
+            }
+            if(count==5)
+            {
+                cout<<"YES"<<endl;
+            }
+           
+        }
+        
+
+    }
+}
